a928bebb4b38c2b691700998b30370480ebbf255
McKayla Washburn <mckayla@hey.com>
2022-07-10 16:18:49 -0600

    hmm

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { serve } from "std/http/server.ts";
 import { hi_friend } from "./build/dev/javascript/nakai/dist/hi.mjs";
 
 function handler(req) {
-	return new Response(hi_friend, {
+	return new Response(hi_friend(), {
 		headers: { "content-type": "text/html" },
 	});
 }
